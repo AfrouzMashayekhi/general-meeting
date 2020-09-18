cace023d9652fb59a3f9b05f47ddd09c4bde44cd
MJafar Mashhadi <raindigital2007@gmail.com>
2020-09-18 07:44:17 +0000

    Hope it fixes it

--- chaincode/generalMeetingSDK/trader.go
+++ chaincode/generalMeetingSDK/trader.go
@@ -5,13 +5,13 @@ import "github.com/afrouzMashaykhi/general-meeting/chaincode/stockMarket"
 // Trader is a shareholder or someone with TraderID who but dividend
 type Trader struct {
 	// Cards is  a list of Trader's cards which have info of dividend and its pays
-	Cards []Card `json:"cards"`
+	Cards []stockMarket.Card `json:"cards"`
 	// TraderID is a unique ID for every trader who register in this market
 	TraderID int `json:"traderID"`
 	//CardStatus []CardStatus `json:"cardstatus"`
 }
 
 // traderRegistration func is called when someone want to join the market
-func traderRegister(cards []Card, traderID int) *Trader {
+func traderRegister(cards []stockMarket.Card, traderID int) *Trader {
 	return &Trader{Cards: cards, TraderID: traderID}
 }
